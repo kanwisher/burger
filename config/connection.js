c58aefa427e6ade5d40ef7e81684c0134780dc54
kanwisher <kanwisher@gmail.com>
2017-04-10 17:33:40 -0400

    fix clearDB issues

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const connection = mysql.createConnection({
 	host : 'us-cdbr-iron-east-03.cleardb.net',
 	user : 'bd9433bd4be5b7',
 	password : '06e640c3',
-	database: 'heroku_b92601014a85b80	'
+	database: 'heroku_b92601014a85b80'
 });
 
 connection.connect(function(err) { //initial connection to app
