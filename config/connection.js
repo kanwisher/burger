b848654b2492c076993786e0ec97993157e08a1d
kanwisher <kanwisher@gmail.com>
2017-04-11 23:42:52 -0400

    fix mysql heroku cleardb?

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,30 @@
 const mysql = require('mysql');
-const connection = mysql.createConnection({
+var dbConfig = {
 	host : 'us-cdbr-iron-east-03.cleardb.net',
 	user : 'bd9433bd4be5b7',
 	password : '06e640c3',
 	database: 'heroku_b92601014a85b80'
-});
+};
 
-connection.connect(function(err) { //initial connection to app
-	if(err) {
-		console.error("error connecting: " + err.stack);
-		return;
-	}
-	console.log("connected as id " + connection.threadId);
-});
+var connection;
+function handleDisconnect() {
+    connection = mysql.createConnection(dbConfig);  // Recreate the connection, since the old one cannot be reused.
+    connection.connect( function onConnect(err) {   // The server is either down
+        if (err) {                                  // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 10000);    // We introduce a delay before attempting to reconnect,
+        }                                           // to avoid a hot loop, and to allow our node script to
+    });                                             // process asynchronous requests in the meantime.
+                                                    // If you're also serving http, display a 503 error.
+    connection.on('error', function onError(err) {
+        console.log('db error', err);
+        if (err.code == 'PROTOCOL_CONNECTION_LOST') {   // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                        // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+handleDisconnect();
 
 module.exports = connection; //allow other documents to access this connection
